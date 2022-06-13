7b7e253343253270011947d350dfbe53b96da80d
ArturoMaiani <100837287+ArturoMaiani@users.noreply.github.com>
2022-06-13 14:07:18 +0200

    Create File_main_circolare.m

---
+++ Circular trajectory/File_main_circolare.m
@@ -0,0 +1,25 @@
+% FILE MAIN CIRCOLARE
+
+clear
+
+%sampling time fed to simulink
+Ts=0.01;
+
+%initial condition for the robot
+q_iniz_circ=[pi;-pi/2;-pi/2];
+
+%duration of the simulation
+Tsim_cir=20;
+
+%output data from simulink
+out=sim('Progetto_robotics_circular') ;
+
+%dynamic plot of the simulation
+animation_circular
+
+
+
+% STROBOSCOPIC VIEW
+% figure
+% 
+%stroboscopio_circular
